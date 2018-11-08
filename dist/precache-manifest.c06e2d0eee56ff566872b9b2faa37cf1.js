9aa65c67645dec58497579e122f54118b434796d
Steven Halase <steven.halase@gmail.com>
2018-11-08 02:08:02 -0700

    Fixed gitignore

---
+++ dist/precache-manifest.c06e2d0eee56ff566872b9b2faa37cf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b45dabc647f4d53cf43a",
+    "url": "/js/chunk-vendors.769556b5.js"
+  },
+  {
+    "revision": "dddfa22323a110f59638",
+    "url": "/js/app.7ec10273.js"
+  },
+  {
+    "revision": "22563399d98ec467fed37cc90b37a1ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddfa22323a110f59638",
+    "url": "/css/app.84194371.css"
+  }
+];
